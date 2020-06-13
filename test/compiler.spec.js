dafb35174b5c35a03aa75d5905141da20ac018c3
al66 <andreas.leinen@imicros.de>
2020-06-13 18:41:57 +0200

    correction json generator

--- test/compiler.spec.js
+++ test/compiler.spec.js
@@ -40,7 +40,7 @@ describe("Test Compiler - parse ruleset", () => {
         exp += "@ environment.date :: [2018-1-21 06:00..2018-2-23 08:00[,>=2018-05-07 09:00 & <= 2020-05-07 09:00 => result := user.confirmed(a) + 1 + 'yes'; rule:= 1 + 1  # rule two # ";
         exp += "@ user.locale :: 'de','es' => result := user.confirmed(a); rule:= 1 + 1  # rule two # ";
         exp += "@ environment.date :: [2018-1-21 06:00..2018-2-23 08:00],>=2018-05-07 09:00 => result := false; rule:= 3 # rule three # ";
-        exp += "@ user.name :: 'Max' => result := 'yes'; rule:= 4  # undefined var # ";
+        exp += "@ user.name :: 'Max' && user.locale :: 'en' => result := 'yes'; rule:= 4  # undefined var # ";
         exp += " @@";
         it("resulting json should return contain expected path", () => {
             return Compiler.toJson(exp).then(ruleset => {
--- test/jsongenerator.spec.js
+++ test/jsongenerator.spec.js
@@ -116,7 +116,7 @@ describe("Test Compiler - parse rule", () => {
         exp += "@ environment.date :: [2018-1-21 06:00..2018-2-23 08:00[,>=2018-05-07 09:00 & <= 2020-05-07 09:00 => result := user.confirmed(a) + 1 + 'yes'; rule:= 1 + 1  # rule two # ";
         exp += "@ user.locale :: 'de','es' => result := user.confirmed(a); rule:= 1 + 1  # rule two # ";
         exp += "@ environment.date :: [2018-1-21 06:00..2018-2-23 08:00],>=2018-05-07 09:00 => result := false; rule:= 3 # rule three # ";
-        exp += "@ user.name :: 'Max' => result := 'yes'; rule:= 4  # undefined var # ";
+        exp += "@ user.name :: 'Max'  && user.locale :: 'en' => result := 'yes'; rule:= 4  # undefined var # ";
         exp += " @@";
         let lexer = new Lexer(exp);
         let parser = new Parser();
